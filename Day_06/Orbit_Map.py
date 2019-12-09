e2fd20a0dc145ef2b795534742edf8b0f7a074a7
Brandon Charest <b.charest019@gmail.com>
2019-12-09 15:44:06 -0500

    Day_06 Part2

--- Day_06/Orbit_Map.py
+++ Day_06/Orbit_Map.py
@@ -1,4 +1,4 @@
-def find_orbits(orbit_data):
+def find_orbits(orbit_data, find_path=False):
     orbits = [x for x in orbit_data.split('\n')]
     orbit_graph = {}
 
@@ -15,9 +15,29 @@ def find_orbits(orbit_data):
         else:
             orbit_graph[planet1] = [planet2]
 
-    return sum([count_orbits(k) for k in orbit_graph.keys()])
+    def dfs_paths(graph, start, goal):
+        stack = [(start, [start])]
+        visited = set()
+        while stack:
+            (vertex, path) = stack.pop()
+            if vertex not in visited:
+                if vertex == goal:
+                    return path
+                visited.add(vertex)
+                if vertex in graph:
+                    for neighbor in graph[vertex]:
+                        stack.append((neighbor, path + [neighbor]))
+
+    # this is pretty ugly :( but it works
+    if find_path:
+        you = dfs_paths(orbit_graph, 'COM', 'YOU')
+        san = dfs_paths(orbit_graph, 'COM', 'SAN')
+        both = [x for x in you if x in san]
+        return (len(you) - len(both) + len(san) - len(both)) - 2
+    else:
+        return sum([count_orbits(k) for k in orbit_graph.keys()])
 
 
 with open('input', 'r') as f:
     data = f.read()
-print(find_orbits(data))
+print(find_orbits(data, find_path=True))
