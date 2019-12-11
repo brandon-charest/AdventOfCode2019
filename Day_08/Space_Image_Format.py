70bff84b8133988f9bd3602e618ee9a9342c01b1
Brandon Charest <b.charest019@gmail.com>
2019-12-11 14:53:55 -0500

    Day_08 Part2

--- Day_08/Space_Image_Format.py
+++ Day_08/Space_Image_Format.py
@@ -1,9 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('input', 'r') as f:
     digits = [int(i) for i in f.read()]
 
-layers = np.array(digits).reshape((-1, 6, 25))
-least_zeros = layers[np.count_nonzero(layers, axis=(1, 2)).argmax()]
-print(np.count_nonzero(least_zeros == 1) * np.count_nonzero(least_zeros == 2))
 
+def decode_image(image_data, height=25, width=6):
+    layers = np.array(image_data).reshape((-1, width, height))
+    least_zeros = layers[np.count_nonzero(layers, axis=(1, 2)).argmax()]
+    print(np.count_nonzero(least_zeros == 1) * np.count_nonzero(least_zeros == 2))
+    return
+
+
+def display_image(image_data, height=25, width=6):
+    layers = np.array(image_data).reshape((-1, width, height))
+    decoded_layer = layers[0]
+    for layer in layers:
+        # If layer contains value of 2 replace it
+        decoded_layer = np.where(decoded_layer != 2, decoded_layer, layer)
+    decoded_layer = decoded_layer.reshape(width, height)
+    plt.imshow(decoded_layer)
+    plt.savefig('output.png')
+    plt.show()
+    return
+
+
+decode_image(digits)
+display_image(digits)
