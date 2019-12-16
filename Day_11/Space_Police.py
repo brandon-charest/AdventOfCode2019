f09c0043e09cd92abb05c7272be3c56439e6897d
Brandon Charest <b.charest019@gmail.com>
2019-12-16 16:06:07 -0500

    Day_11 Part2

--- Day_11/Space_Police.py
+++ Day_11/Space_Police.py
@@ -1,6 +1,8 @@
 from IntCode_Machine import IntCodeMachine
 from IntCode_Machine import OP_TYPE
 from collections import defaultdict
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def paint(code, start):
@@ -29,3 +31,18 @@ with open('input', 'r') as f:
         code_arr = [int(i) for i in line.split(',')]
 
 print("Painted:", len(paint(code_arr, start=0)))
+
+image = paint(code_arr, start=1)
+xs = [x for x, y in image.keys()]
+ys = [y for x, y in image.keys()]
+max_x = max(xs) + 1
+max_y = max(ys) + 1
+
+grid = np.zeros([max_y,max_x])
+for y in range(0, max_y):
+    for x in range(0, max_x):
+        grid[y][x] = image[(x, y)]
+plt.imshow(grid)
+plt.savefig('output.png')
+plt.show()
+
